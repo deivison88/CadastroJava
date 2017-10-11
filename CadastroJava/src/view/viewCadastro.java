de308f1d4ac74b023313757e65a578214eca78dc
Deivison S. Pereira <github@deivison88>
2017-10-10 22:00:21 -0300

    Preenchimento da comboBox categoria

--- CadastroJava/src/view/viewCadastro.java
+++ CadastroJava/src/view/viewCadastro.java
@@ -5,6 +5,9 @@
  */
 package view;
 
+import model.bean.Categoria;
+import model.dao.CategoriaDAO;
+
 /**
  *
  * @author Administrador
@@ -13,11 +16,20 @@ public class viewCadastro extends javax.swing.JFrame {
 
     /**
      * Creates new form viewCadastro
+     * Método Construtor
      */
     public viewCadastro() {
         initComponents();
+        preencherComboBoxCategorias();
     }
 
+    private void preencherComboBoxCategorias(){
+        CategoriaDAO catDAO = new CategoriaDAO();
+        for(Categoria cat: catDAO.findAll()){
+            jComboBoxCategorias.addItem(cat);
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
